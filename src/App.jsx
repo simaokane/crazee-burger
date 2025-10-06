12e321fe6b7efdcbbc95bb38ffa2a08d741cf6f5
simaokane <simadrien@gmail.com>
2025-10-07 00:32:13 +0200

    ✨ F02: APP > install routin system

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
+import { Route, Routes } from "react-router-dom"
 import LoginPage from "./components/pages/login/LoginPage"
+import OrderPage from "./components/pages/order/OrderPage"
+import ErrorPage from "./components/pages/error/ErrorPage"
 
 
 function App() {
-  return <LoginPage />  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Bonjour Adrien Sima</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <Routes>
+    <Route path="/" element={<LoginPage/>}/>   
+    <Route path="/order" element={<OrderPage/>}/>   
+    <Route path="*" element={<ErrorPage/>}/>   
+  </Routes> 
+  )  
 }
 
 export default App
